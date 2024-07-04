5ef4e83fe41f0c46ef9c75470dffde4686f63dea
ESTEVAM PACHECO <estevam.p@edu.pucrs.br>
2024-07-04 11:08:07 -0300

    changes

--- aa3.asm
+++ aa3.asm
@@ -28,23 +28,55 @@ inicio:
 
 loop:
 	# Loop que percorre até o fim do vetor
-	add	$t1, $t1, 1
-	blt 	$t1, $t0, loop
-	
-	
-adicaoDeVetores:
 
+	lw	$s0, 0($t3) # A
+	lw	$s1, 0($t5) # B
 	
+	add	$s0, $s0, $s1	# Guarda no s0 (A): A+B
+	sw	$s0 ($t7)	# Guarda no C ($t7): A
 	
+	blt	$s0, $zero, pula
+	# Aqui somar
+	
+	pula:
+	lw	$s0, 0($t3) # A
+	lw	$s1, 0($t5) # B
+	
+	sub	$s0, $s0, $s1	# Guarda no s0 (A): A-B
+	sw	$s0 ($t9)	# Guarda no D ($t9): A
+	
+	blt	$s0, $zero, pula2
+	# Aqui somar
+	
+	pula2:		
+	
+	#Adição de endereços
 	add $t3, $t3, 4
 	add $t5, $t5, 4
 	add $t7, $t7, 4
 	add $t9, $t9, 4
+	#Adição de endereços
+	
+	# Adição do iterador
+	add	$t1, $t1, 1
+
+	# Compara iterador ($t1) com tamanhoVetor ($t0)
+	blt 	$t1, $t0, loop
+	
+
+multiplicacao:
+
+	
+
+fim: j fim
+
 	
 .data
-tamanhoVetor: .word 8		#t0
-iterador: .word 0		#t1
-A: .word 2 3 4 5 6 7 8 9	
-B: .word 2 3 4 5 6 7 8 9	
+tamanhoVetor: .word 8		
+iterador: .word	0		
+A: .word 10 20 30 40 50 60 70 80	
+B: .word 5 10 15 20 25 30 35 40	
 C: .word 0 0 0 0 0 0 0 0	
-D: .word 0 0 0 0 0 0 0 0 			
+D: .word 0 0 0 0 0 0 0 0
+somaPositivosC: .word 0
+somaPositivosD: .word 0 			
